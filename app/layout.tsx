e06f0537320c5165ad5c10840e30809e4496d29b
mahfooz <mahfooz720946@gmail.com>
2024-05-17 16:10:43 +0530

    fix: sovled hydration erros
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
+    <html lang="en" >
         <body className={inter.className}>
         <ThemeProvider
           attribute="class"
