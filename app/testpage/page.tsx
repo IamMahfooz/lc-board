f8bbdb4372326e6e01cadfa9fce31b588e57e273
mahfooz <mahfooz720946@gmail.com>
2024-06-03 00:35:39 +0530

    fix: mobile view of whiteboard
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -18,8 +18,8 @@ function Search(){  const searchParams = useSearchParams()
   return (
     <div>
       <title>{pageTitle}</title>
-      <div className="sticky z-40 flex h-[calc(100vh-3.5rem)] w-screen">
-        <div className="grid grid-cols-[30%_70%] h-full divide-x">
+      <div className="sticky z-40 flex w-screen">
+        <div className="grid grid-cols-[30%_70%] h-[calc(100vh-4rem)] w-screen divide-x">
           <div className="px-4 w-full overflow-auto">
             <ProblemStat />
           </div>
