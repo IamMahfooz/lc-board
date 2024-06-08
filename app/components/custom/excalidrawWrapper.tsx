f571f4a6d2a59e82b22c28c0c1cd2f06128cca28
mahfooz <mahfooz720946@gmail.com>
2024-06-08 16:25:27 +0530

    feat: immporoved caching mechanism using getSceneVersion
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- app/components/custom/excalidrawWrapper.tsx
+++ app/components/custom/excalidrawWrapper.tsx
@@ -1,15 +1,21 @@
-import {Excalidraw, MainMenu, serializeAsJSON, WelcomeScreen} from "@excalidraw/excalidraw"
+import {Excalidraw, MainMenu, serializeAsJSON, WelcomeScreen, getSceneVersion} from "@excalidraw/excalidraw"
 import {ExcalidrawElement} from "@excalidraw/excalidraw/types/element/types"
 import {AppState, BinaryFiles} from "@excalidraw/excalidraw/types/types"
 
 const ExcalidrawWrapper: React.FC<{ probID: string | string[] }> = ({probID}) => {
+
+    let sceneVersion = 0;
     const onchange = (elements: readonly ExcalidrawElement[], appState: AppState, files: BinaryFiles) => {
-        const content = serializeAsJSON(elements, appState, files, "local")
-        localStorage.setItem(`excalidraw-${probID}`, content)
-        console.log("saved the content")
+        if (getSceneVersion(elements) !== sceneVersion) {
+            sceneVersion = getSceneVersion(elements)
+            const content = serializeAsJSON(elements, appState, files, "local")
+            localStorage.setItem(`excalidraw-${probID}`, content)
+            console.log("saved the content with s-v", sceneVersion)
+        }
     }
     const renderInitialData = () => {
         const content = localStorage.getItem(`excalidraw-${probID}`)
+        sceneVersion = 0
         if (content != null) {
             return JSON.parse(content)
         } else {
