9ec0df3eb75fee2326839ed56418d15f1458000f
mahfooz <mahfooz720946@gmail.com>
2024-06-09 01:28:28 +0530

    feat: added login page with zod validation
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

---
+++ app/components/custom/login-form.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import {z} from "zod"
+import {useRouter} from "next/navigation"
+
+const loginFormSchema = z.object({
+    email: z.string().email({message: 'Email is required'}),
+    password: z.string().min(8, {message: "Password must be at least 8 characters long"}),
+})
+
+export default function LoginProfileForm(form: any) {
+    const router = useRouter();
+    return function onSubmit(values: z.infer<typeof loginFormSchema>) {
+        router.push(`/`)
+    }
+}
