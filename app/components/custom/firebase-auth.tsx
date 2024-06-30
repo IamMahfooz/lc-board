0c52ab89beb9d231000bcb4d6f7058861ac97eb7
mahfooz <mahfooz720946@gmail.com>
2024-07-01 01:38:52 +0530

    feat: added database configs
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- app/components/custom/firebase-auth.tsx
+++ app/components/custom/firebase-auth.tsx
@@ -1,6 +1,9 @@
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 
+import {getFirestore} from 'firebase/firestore';
+
+
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
@@ -18,4 +21,5 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 
