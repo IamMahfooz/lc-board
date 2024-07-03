cae1ef40a83abbbcd639d0197a95dfb3e1508cfb
mahfooz <mahfooz720946@gmail.com>
2024-07-03 23:38:49 +0530

    fix: fixing config env problems [1-try]
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- app/components/custom/firebase-auth.tsx
+++ app/components/custom/firebase-auth.tsx
@@ -8,13 +8,13 @@ import {getFirestore} from 'firebase/firestore';
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 // hide this before making public
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: `${process.env.API_KEY}`,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
+    projectId: `${process.env.PROJECT_ID}`,
+    storageBucket: `${process.env.STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+    appId: `${process.env.APP_ID}`,
+    measurementId: `${process.env.MEASUREMENT_ID}`
 };
 
 // Initialize Firebease
